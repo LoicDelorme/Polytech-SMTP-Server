874bcf0fb12ed0d4fa200645a3f0aebd106079a8
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-04-05 10:48:48 +0200

    Add new command result

---
+++ src/fr/polytech/smtp/server/commands/results/InvalidParametersCommandResult.java
@@ -0,0 +1,39 @@
+package fr.polytech.smtp.server.commands.results;
+
+import fr.polytech.smtp.server.requests.MailDropRequest;
+
+/**
+ * This class represents an invalid parameter(s) command result.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public class InvalidParametersCommandResult extends CommandResult {
+
+	/**
+	 * The message.
+	 */
+	private static final String MESSAGE = "501 Invalid parameter(s) for the current command";
+
+	/**
+	 * Create an invalid parameter(s) command result.
+	 */
+	public InvalidParametersCommandResult() {
+		super(CommandResultStatus.ERROR);
+	}
+
+	/**
+	 * Create an invalid parameter(s) command result.
+	 * 
+	 * @param mailDropRequest
+	 *            The mail drop request.
+	 */
+	public InvalidParametersCommandResult(MailDropRequest mailDropRequest) {
+		super(CommandResultStatus.ERROR, mailDropRequest);
+	}
+
+	@Override
+	public String toString() {
+		return MESSAGE;
+	}
+}
